16cf720f7528ac3d11541abcdbb5dd1d0fedbed0
Frederic Blanc-Brude <frederic.blanc-brude@edhec.edu>
2017-01-27 15:59:58 +0800

    initial commit

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Production and farm value of maple products in Canada
+#'
+#' @source EDHECinfra database, 27jan17
+#'  maple products, annual. \url{http://www5.statcan.gc.ca/cansim/}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{Year}{2000 to 2015}
+#'  \item{Syrup}{Maple products expressed as syrup, total in thousands of gallons.}
+#'  \item{CAD}{Gross value of maple products in thousands of Canadian dollars.}
+#'  \item{Region}{Postal code abbreviation for territory or province.}
+#' }
+#' @examples
+#' \dontrun{
+#'  infra_data_companies_current
+#' }
+"infra_data_companies_current"
